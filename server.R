8ca8401bbd86f849a28ea56fe0a6ed063fa29ae3
Mirjam Moerbeek <67948954+MirjamMoerbeek@users.noreply.github.com>
2024-06-12 13:59:03 +0200

    Update server.R

--- server.R
+++ server.R
@@ -10,7 +10,7 @@ function(input, output, session) {
     ICC=input$ICC1                    # intraclass correlation coefficient
     CAC=input$CAC1                    # cluster autocorrelation
 
-    nr.subj=input$nr.subj1             # number of subjects per cluster-period
+    nr.subj=input$nr.subj1            # number of subjects per cluster-period
     min.nr.per=input$nr.per1[1]       # minimum number of periods to be evaluated
     max.nr.per=input$nr.per1[2]       # maximum number of periods to be evaluated
 
@@ -44,7 +44,7 @@ function(input, output, session) {
       XX.B=cbind(rep(1,nr.per),diag(1,nrow=nr.per)[,2:nr.per],sequence.B[1:nr.per])
       var.gamma1=solve((t(XX.A)%*%VVinv%*%XX.A+t(XX.B)%*%VVinv%*%XX.B))                                       # covariance matrix
       costs=costs.c+costs.s*nr.subj*nr.per+costs.x*(nr.per-1)                                                 # costs in each cluster for current sequence    
-      N=floor(0.5*budget/costs)
+      N=floor(0.5*budget/costs)                                                                               # N=K/2= number of clusters per sequence
       var.gamma2=solve((N*t(XX.A)%*%VVinv%*%XX.A+N*t(XX.B)%*%VVinv%*%XX.B))                                   # covariance matrix
       results[ii,]=(c(nr.per,var.gamma1[nr.per+1,nr.per+1],NA,2*N,costs*2*N,var.gamma2[nr.per+1,nr.per+1],NA))
     }
@@ -57,7 +57,7 @@ function(input, output, session) {
     plot=plot_ly(results, x = ~results[,1], y = ~results[,3], mode='lines+markers',type="scatter",marker=list(size=10,color='red'),line=list(width=0.1,color='black'),name="fixed number of clusters") 
       plot=plot %>% add_lines(y=~results[,7],key = ~key,mode='lines',type="scatter",marker=list(size=10,color='blue'),line=list(color='black'),name="budgetary constraint")
       plot=plot %>% layout(dragmode = "select",
-             xaxis = list(title = "Number of time periods", range = c(0, 12.5), showgrid = F,zeroline=TRUE,showline = TRUE),
+             xaxis = list(title = "Number of time periods", range = c(0, max.nr.per+0.5), showgrid = F,zeroline=TRUE,showline = TRUE),
              yaxis = list(title = "Relative Efficiency", range=c(0,1.05),showgrid = T,zeroline=TRUE,showline = TRUE))
   })
  
@@ -103,7 +103,7 @@ function(input, output, session) {
       XX.B=cbind(rep(1,nr.per),diag(1,nrow=nr.per)[,2:nr.per],sequence.B[1:nr.per])
       var.gamma1=solve((t(XX.A)%*%VVinv%*%XX.A+t(XX.B)%*%VVinv%*%XX.B))                                       # covariance matrix
       costs=costs.c+costs.s*nr.subj*nr.per+costs.x*(nr.per-1)                                                 # costs in each cluster for current sequence    
-      N=floor(0.5*budget/costs)
+      N=floor(0.5*budget/costs)                                                                               # N=K/2= number of clusters per sequence
       var.gamma2=solve((N*t(XX.A)%*%VVinv%*%XX.A+N*t(XX.B)%*%VVinv%*%XX.B))                                   # covariance matrix
       results[ii,]=(c(nr.per,var.gamma1[nr.per+1,nr.per+1],NA,2*N,costs*2*N,var.gamma2[nr.per+1,nr.per+1],NA))
     }
@@ -112,7 +112,7 @@ function(input, output, session) {
     results[min.nr.per:max.nr.per,7]=min(results[min.nr.per:max.nr.per,6])/results[min.nr.per:max.nr.per,6]   # RE given budgetary constraint
     results=as.data.frame(results)
     results=round(results[min.nr.per:max.nr.per,c(1,3,4,5,7)],3)
-    colnames(results)=c("number of periods","RE for fixed number of clusters","N for budgetary constraint", "costs for budgetary constraint", "RE for budgetary constraint")    # column names in results matrix 
+    colnames(results)=c("number of periods","RE for fixed number of clusters","K for budgetary constraint", "costs for budgetary constraint", "RE for budgetary constraint")    # column names in results matrix 
     datatable(results,rownames=FALSE, options = list(pageLength = 25,dom = 'tl'))
   })
   
@@ -160,7 +160,7 @@ function(input, output, session) {
       var.gamma1=solve((t(XX.A)%*%VVinv%*%XX.A+t(XX.B)%*%VVinv%*%XX.B))                                                 # covariance matrix
       
       costs=costs.c+costs.s*nr.subj*nr.per+costs.x*nr.switches                                                          # costs in each cluster of current sequence    
-      N=floor(0.5*budget/costs)
+      N=floor(0.5*budget/costs)                                                                                         # N=K/2= number of clusters per sequence
       var.gamma2=solve(N*(t(XX.A)%*%VVinv%*%XX.A+N*t(XX.B)%*%VVinv%*%XX.B))                                             # covariance matrix
       
       results[ii,]=(c(ii,nr.switches,var.gamma1[nr.per+1,nr.per+1],NA,2*N,costs*2*N,var.gamma2[nr.per+1,nr.per+1],NA))
@@ -173,7 +173,7 @@ function(input, output, session) {
     sequences[sequences==1]="B"
     order=do.call(paste,sequences)
     results=data.frame(order[1:(length(order)/2)],results[,c(2,4,5,6,8)])
-    colnames(results)=c("order of treatments","nr.switches","RE for fixed number of clusters","N for budgetary constraint", "costs for budgetary constraint", "RE for budgetary constraint")  # column names in results matrix 
+    colnames(results)=c("order of treatments","nr.switches","RE for fixed number of clusters","K for budgetary constraint", "costs for budgetary constraint", "RE for budgetary constraint")  # column names in results matrix 
     datatable(results,rownames=FALSE, options = list(pageLength = 0.5*2^nr.per,dom = 'tl',order=list(1, 'asc')))
   })
   
